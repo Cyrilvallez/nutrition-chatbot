2e62b2b820f0ef55100d8a2ae7abd4b0c1d29f43
Cyril Vallez <cyril.vallez@gmail.com>
2023-11-15 16:31:49 +0100

    User-free version of the chatbot with session state

---
+++ chatbot_user_free.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=nutrition-chatbot
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=3-00:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50G
+#SBATCH --partition=nodes
+#SBATCH --gres=gpu:a100:2
+#SBATCH --chdir=/cluster/raid/home/vacy/nutrition-chatbot
+
+# Initialize the shell to use local conda
+eval "$(conda shell.bash hook)"
+
+# Activate (local) env
+conda activate llm
+
+# Note: the -u option is absolutely necesary here to force the flush of the link 
+# to connect to the app!
+python3 -u chatbot_user_free.py
+
+conda deactivate
